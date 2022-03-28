3123ab5ac9d5994714603f76601a1b6bb2da8f35
Christoph Rehmann <1273098+crehmann@users.noreply.github.com>
2022-03-28 19:07:55 +0000

    remove bongo cat

--- boards/arm/digiti/custom_status_screen.c
+++ boards/arm/digiti/custom_status_screen.c
@@ -24,12 +24,6 @@ static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_output_status output_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BONGO_CAT)
-#include "widgets/bongo_cat_widget.h"
-static struct zmk_widget_bongo_cat bongo_widget;
-#endif
-
-
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
 
@@ -53,12 +47,6 @@ lv_obj_t *zmk_display_status_screen() {
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -8);
-#endif
-
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BONGO_CAT)
-    LOG_DBG("Bongo");
-    zmk_widget_bongo_cat_init(&bongo_widget, screen);
-    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
 #endif
 
     return screen;
