d9169933de8715fdf4c3bb4b4b31780ef82ec559
Christoph Rehmann <1273098+crehmann@users.noreply.github.com>
2022-07-13 19:38:42 +0000

    separated peripheral and output widget

--- boards/arm/digiti/custom_status_screen.c
+++ boards/arm/digiti/custom_status_screen.c
@@ -7,6 +7,7 @@
 #include "widgets/battery_status.h"
 #include "widgets/layer_status.h"
 #include "widgets/output_status.h"
+#include "widgets/peripheral_status.h"
 
 #include <logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -24,20 +25,19 @@ static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_output_status output_status_widget;
 #endif
 
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
+static struct zmk_widget_peripheral_status peripheral_status_widget;
+#endif
+
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
-
-
-
     screen = lv_obj_create(NULL, NULL);
-
     
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
     LOG_DBG("Battery");
     zmk_widget_battery_status_init(&battery_status_widget, screen);
     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_LEFT, 14, 8);
 #endif
-
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
@@ -47,6 +47,11 @@ lv_obj_t *zmk_display_status_screen() {
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -8);
+#endif
+
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
+    zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT, -8, 8);
 #endif
 
     return screen;
--- boards/arm/digiti/widgets/output_status.c
+++ boards/arm/digiti/widgets/output_status.c
@@ -3,15 +3,18 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <kernel.h>
 #include <bluetooth/services/bas.h>
 
 #include <logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
+#include <zmk/display.h>
 #include "output_status.h"
 #include <zmk/event_manager.h>
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/events/ble_active_profile_changed.h>
+#include <zmk/events/endpoint_selection_changed.h>
 #include <zmk/usb.h>
 #include <zmk/ble.h>
 #include <zmk/endpoints.h>
@@ -32,29 +35,35 @@ LV_IMG_DECLARE(output_ble_dis_3_img);
 LV_IMG_DECLARE(output_ble_dis_4_img);
 LV_IMG_DECLARE(output_ble_dis_5_img);
 LV_IMG_DECLARE(output_usb_img);
-LV_IMG_DECLARE(output_ble_adv_img);
-LV_IMG_DECLARE(output_ble_con_img);
-LV_IMG_DECLARE(output_ble_dis_img);
 
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-void set_status_symbol(lv_obj_t *icon) {
-    bool active_profile_connected = zmk_ble_active_profile_is_connected();
-    bool active_profie_bonded = !zmk_ble_active_profile_is_open();
+struct output_status_state {
+    enum zmk_endpoint selected_endpoint;
+    bool active_profile_connected;
+    bool active_profile_bonded;
+    uint8_t active_profile_index;
+};
 
-#if defined(CONFIG_ZMK_SPLIT_BLE_ROLE_CENTRAL)
-    uint8_t active_profile_index = zmk_ble_active_profile_index();
-    enum zmk_endpoint selected_endpoint = zmk_endpoints_selected();
+static struct output_status_state get_state(const zmk_event_t *_eh) {
+    return (struct output_status_state){.selected_endpoint = zmk_endpoints_selected(),
+                                        .active_profile_connected =
+                                            zmk_ble_active_profile_is_connected(),
+                                        .active_profile_bonded = !zmk_ble_active_profile_is_open(),
+                                        .active_profile_index = zmk_ble_active_profile_index()};
+    ;
+}
 
-    switch (selected_endpoint) {
+void set_status_symbol(lv_obj_t *icon, struct output_status_state state) {
+    switch (state.selected_endpoint) {
     case ZMK_ENDPOINT_USB:
         lv_img_set_src(icon, &output_usb_img);
         break;
     case ZMK_ENDPOINT_BLE:
-        if (active_profie_bonded) {
-            if (active_profile_connected) {
-                switch (active_profile_index) {
+        if (state.active_profile_bonded) {
+            if (state.active_profile_connected) {
+                switch (state.active_profile_index) {
                 case 0:
                     lv_img_set_src(icon, &output_ble_con_1_img);
                     break;
@@ -72,7 +81,7 @@ void set_status_symbol(lv_obj_t *icon) {
                     break;
                 }
             } else {
-                switch (active_profile_index) {
+                switch (state.active_profile_index) {
                 case 0:
                     lv_img_set_src(icon, &output_ble_dis_1_img);
                     break;
@@ -91,7 +100,7 @@ void set_status_symbol(lv_obj_t *icon) {
                 }
             }
         } else {
-            switch (active_profile_index) {
+            switch (state.active_profile_index) {
                 case 0:
                     lv_img_set_src(icon, &output_ble_adv_1_img);
                     break;
@@ -111,42 +120,35 @@ void set_status_symbol(lv_obj_t *icon) {
         }
         break;
     }
-#endif
-#if !defined(CONFIG_ZMK_SPLIT_BLE_ROLE_CENTRAL)
-    if (active_profie_bonded) {
-        if (active_profile_connected) {
-            lv_img_set_src(icon, &output_ble_con_img);
-        } else {
-            lv_img_set_src(icon, &output_ble_dis_img);
-        }
-    } else {
-        lv_img_set_src(icon, &output_ble_adv_img);
-    }
-#endif
 }
+
+static void output_status_update_cb(struct output_status_state state) {
+    struct zmk_widget_output_status *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_status_symbol(widget->obj, state); }
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_output_status, struct output_status_state,
+                            output_status_update_cb, get_state)
+ZMK_SUBSCRIPTION(widget_output_status, zmk_endpoint_selection_changed);
+
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+ZMK_SUBSCRIPTION(widget_output_status, zmk_usb_conn_state_changed);
+#endif
+#if defined(CONFIG_ZMK_BLE)
+ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
+#endif
 
 int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent) {
     widget->obj = lv_img_create(parent, NULL);
+
     lv_obj_set_size(widget->obj, 38, 53);
-    set_status_symbol(widget->obj);
+
     sys_slist_append(&widgets, &widget->node);
+
+    widget_output_status_init();
     return 0;
 }
 
 lv_obj_t *zmk_widget_output_status_obj(struct zmk_widget_output_status *widget) {
     return widget->obj;
 }
-
-int output_status_listener(const zmk_event_t *eh) {
-    struct zmk_widget_output_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_status_symbol(widget->obj); }
-    return ZMK_EV_EVENT_BUBBLE;
-}
-
-ZMK_LISTENER(widget_output_status, output_status_listener)
-#if defined(CONFIG_USB)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_usb_conn_state_changed);
-#endif
-#if defined(CONFIG_ZMK_BLE)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
-#endif
